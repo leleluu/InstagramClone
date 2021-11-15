617b2ac75cf76cc28ba4801410dbb2802b023620
Lele Luu <leleluu@gmail.com>
2021-11-15 17:33:11 +0100

    Set up project

---
+++ InstagramClone/Controller/MainTabController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+    
+}
