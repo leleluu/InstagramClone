42e0c81fd7d971279f298b5b58da1df6dbf3c450
Lele Luu <leleluu@gmail.com>
2021-11-16 22:50:30 +0100

    Add username button

--- InstagramClone/View/FeedCell.swift
+++ InstagramClone/View/FeedCell.swift
@@ -12,18 +12,37 @@ class FeedCell: UICollectionViewCell {
         return iv
     }()
 
+    private lazy var userNameButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("venom", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 13)
+        button.addTarget(self, action: #selector(didTapUserName), for: .touchUpInside)
+        return button
+    }()
+
     // MARK: - Lifecycle
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .white
+
         addSubview(profileImageView)
         profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 12)
         profileImageView.setDimensions(height: 40, width: 40)
         profileImageView.layer.cornerRadius = 40 / 2
+
+        addSubview(userNameButton)
+        userNameButton.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 8)
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Actions
+
+    @objc func didTapUserName() {
+        print("did tap username")
     }
 }
