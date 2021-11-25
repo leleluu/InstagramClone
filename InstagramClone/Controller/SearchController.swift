8c022bd849bd71e11bbde178b456d05304be0882
Lele Luu <leleluu@gmail.com>
2021-11-25 19:14:39 +0100

    Fetch and display user info on SearchController

--- InstagramClone/Controller/SearchController.swift
+++ InstagramClone/Controller/SearchController.swift
@@ -4,28 +4,43 @@ class SearchController: UITableViewController{
 
     private let reuseIdentifier = "UserCell"
 
+    // MARK: - Properties
+    private var users = [User]()
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
+        fetchUsers()
     }
 
     // MARK: - Helpers
-
     func configureTableView() {
         view.backgroundColor = .white
         tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.rowHeight = 64
     }
 
+    // MARK: - API
+
+    func fetchUsers() {
+        UserService.fetchUsers { users in
+            self.users = users
+            self.tableView.reloadData()
+        }
+    }
+
 }
 
 // MARK: - UITableviewDataSource
+
 extension SearchController{
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return users.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
+        cell.viewModel = UserCellViewModel(user: users[indexPath.row])
         return cell
     }
 }
--- InstagramClone/View/UserCell.swift
+++ InstagramClone/View/UserCell.swift
@@ -1,7 +1,14 @@
 import UIKit
 
 class UserCell: UITableViewCell {
+
     // MARK: - Properties
+
+    var viewModel: UserCellViewModel? {
+        didSet {
+            configure()
+        }
+    }
 
     private let profileImageView: UIImageView = {
         let iv = UIImageView()
@@ -48,6 +55,16 @@ class UserCell: UITableViewCell {
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Helpers
+
+    func configure() {
+        guard let viewModel = viewModel else { return }
+
+        profileImageView.sd_setImage(with: viewModel.profileImageUrl)
+        usernameLabel.text = viewModel.username
+        fullnameLabel.text = viewModel.fullname
     }
 
 }
