dff88e793592b48bb6720a11ab2716b1151c2612
Lele Luu <leleluu@gmail.com>
2021-11-25 18:39:15 +0100

    Set up SearchController UI

--- InstagramClone/Controller/SearchController.swift
+++ InstagramClone/Controller/SearchController.swift
@@ -1,10 +1,31 @@
 import UIKit
 
-class SearchController: UIViewController {
+class SearchController: UITableViewController{
+
+    private let reuseIdentifier = "UserCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemOrange
+        configureTableView()
     }
 
+    // MARK: - Helpers
+
+    func configureTableView() {
+        view.backgroundColor = .white
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 64
+    }
+
+}
+
+// MARK: - UITableviewDataSource
+extension SearchController{
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        return cell
+    }
 }
