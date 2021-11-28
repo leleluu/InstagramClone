6b8401e5af87cd9999f77059ddd59cb476d35cc8
Lele Luu <leleluu@gmail.com>
2021-11-28 10:48:51 +0100

    Set up notification model

---
+++ InstagramClone/Model/Notifications.swift
@@ -0,0 +1,35 @@
+import Firebase
+
+
+enum NotificationType: Int {
+    case like
+    case follow
+    case comment
+
+    var notificationMessage: String {
+        switch self {
+        case .like: return " liked your post."
+        case .follow: return " started following you."
+        case .comment: return " commented on your post."
+        }
+    }
+}
+
+struct Notification {
+
+    let uid: String
+    let postImageUrl: String?
+    let postId: String?
+    let timestamp: Timestamp
+    let type: NotificationType
+    let id: String
+
+    init(dictionary: [String: Any]) {
+        self.uid = dictionary["uid"] as? String ?? ""
+        self.postId = dictionary["postId"] as? String ?? ""
+        self.postImageUrl = dictionary["postImageUrl"] as? String ?? ""
+        self.id = dictionary["id"] as? String ?? ""
+        self.timestamp = dictionary["timestamp"] as? Timestamp ?? Timestamp(date: Date())
+        self.type = NotificationType(rawValue: dictionary["type"] as? Int ?? 0) ?? .like
+    }
+}
