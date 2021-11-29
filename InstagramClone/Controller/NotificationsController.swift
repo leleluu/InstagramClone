d647cc2434428d6df33ce521b7924388e96422c4
Lele Luu <leleluu@gmail.com>
2021-11-29 18:59:48 +0100

    Show loader on notifications screen

--- InstagramClone/Controller/NotificationsController.swift
+++ InstagramClone/Controller/NotificationsController.swift
@@ -84,7 +84,10 @@ extension NotificationsController {
 
 extension NotificationsController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        showLoader(true)
         UserService.fetchUser(withUid: notifications[indexPath.row].uid) { user in
+            self.showLoader(false)
+
             let controller = ProfileController(user: user)
             self.navigationController?.pushViewController(controller, animated: true)
         }
@@ -95,20 +98,27 @@ extension NotificationsController {
 
 extension NotificationsController: NotificationCellDelegate {
     func cell(_ cell: NotificationCell, wantsToFollow uid: String) {
+        showLoader(true)
         UserService.follow(uid: uid) { _ in
+            self.showLoader(false)
             cell.viewModel?.notification.userIsFollowed.toggle()
         }
     }
 
     func cell(_ cell: NotificationCell, wantsToUnfollow uid: String) {
+        showLoader(true)
         UserService.unfollow(uid: uid) { _ in
+            self.showLoader(false)
             cell.viewModel?.notification.userIsFollowed.toggle()
         }
 
     }
 
     func cell(_ cell: NotificationCell, wantsToViewPost postId: String) {
+        showLoader(true)
+
         PostService.fetchPost(withPostId: postId) { post in
+            self.showLoader(false)
             let controller = FeedController(collectionViewLayout: UICollectionViewFlowLayout())
             controller.post = post
             self.navigationController?.pushViewController(controller, animated: true)
